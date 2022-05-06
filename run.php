06724f1f26c7f4f31262e2f2a740156b83a03601
rinaamalia27 <105028484+rinaamalia27@users.noreply.github.com>
2022-05-06 10:53:34 +0700

    Update run.php

--- run.php
+++ run.php
@@ -6,7 +6,7 @@ require_once __DIR__.'/userAgent.php';
 require __DIR__ . '/smshub.php';
 
 //APIKEY SMSHUB
-$key = 'https://forward.oneaset.co.id/management/s/7BfQFf';
+$key = 'https://smshub.org/stubs/handler_api.php';
 
 if($key == null) {
     die('Masukan apikey di line 9 goblog!!');
